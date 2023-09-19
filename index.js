e80fdf901486e22a92c9aa939ffd855b569fa26e
Shaun Omari <Shaunomari72@gmail.com>
2023-09-19 21:36:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1= 2
+const num2= 31
+const multiply = num1 * num2
 
-// Write your code here
+function random() {
+    return Math.floor(Math.random()* 1000) ;
+}
+const num3 = 40
+const num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(2,4,6,7,12,20)
